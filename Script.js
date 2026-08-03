b62738ecfc021f60ae559fb46e26249dee4f1b22
a2zserviceskodad-hash <a2zserviceskodad@gmail.com>
2026-08-03 20:41:00 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-function changeMessage() {
-    const heading = document.getElementById("heading");
-
-    heading.innerHTML = "🎉 You clicked the button!";
-
-    document.body.style.background =
-        "linear-gradient(135deg, #ff9a9e, #fad0c4)";
-}
